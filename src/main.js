7749c357f8d11ab2f5a00be35563b22c967a54c5
Dmitriy Ukhin <dukhin@teledes.com.ua>
2020-01-10 14:10:29 +0200

    update mission engine

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 var missionFactory = require("missionFactory");
 
 module.exports.loop = function () {
-    let missions = [];
-    //missionFactory.create("A", {targetId: "5bbcab2c9099fc012e63305b", id: 1, requiredCreeps: 5})
-    if(Game.getObjectById("5bbcab2c9099fc012e63305b").level < 8){
-        missions.push(missionFactory.create("A", {targetId: "5bbcab2c9099fc012e63305b", id: 1, requiredCreeps: 5}));
-    }
-
-    missions.forEach(mission => mission.execute());
+    Memory.missions.forEach(mission => missionFactory.create(mission).execute());
 };
 
--- src/missionFactory.js
+++ src/missionFactory.js
@@ -1,73 +1,93 @@
 /**
- * @param {{targetId: string, id: number, requiredCreeps: number}} options
+ * @param {number} id mission ID in memory.root
  * @description upgrade controller
  * */
-function MissionA(options){
-    this.namePrefix = "A";
-    this.options = options;
-    this.quantityCreeps = 0;
+function MissionUpdateController(id){
+    this.namePrefix = "uc";
+    this.id = id;
+    Memory.missions[id].processData = {};
 }
 
-MissionA.name = "A";
+MissionUpdateController.name = "uc";
 
-MissionA.prototype.execute = function () {
-    let target = Game.getObjectById(this.options.targetId);
-    let targetRoom = target.room;
-    /**
-     * @type {StructureSpawn}*/
-    let spawnerRoom = null;
+MissionUpdateController.prototype.execute = function () {
+    let missionParams = Memory.missions[this.id];
+    if(missionParams.isSuccessful){
+        Game.notify("UpdateController is successfully, remove mission from memory");
+        return;
+    }
+    /**@type {StructureController}*/
+    let target = Game.getObjectById(missionParams.targetId);
+
+    if(target.level === 8){
+        missionParams.isSuccessful = true;
+        return;
+    }
+
+    /**@type {StructureSpawn}*/
+    let spawnerRoom = Game.getObjectById(missionParams.processData.spawnId);
 
     if(spawnerRoom == null) {
         for (let i in Game.spawns) {
             if (target.room.name === Game.spawns[i].room.name) {
                 spawnerRoom = Game.spawns[i];
+                missionParams.processData.spawnId = spawnerRoom.id;
             }
         }
 
-        if(spawnerRoom == null){
-            Game.notify("I can't find spawner in "+targetRoom.name);
+        if (spawnerRoom == null) {
+            Game.notify("Spawner not found in " + target.room.name);
             return;
         }
-    }
 
-    if(this.quantityCreeps < this.options.requiredCreeps){
-        spawnerRoom.spawnCreep([MOVE, WORK, CARRY], (this.namePrefix+"-"+this.options.id+"-"+Math.floor(Math.random()*100)), {});
-    }
 
-    let quantity = 0;
-    for(let i in Game.creeps){
-        let creep = Game.creeps[i];
-        let name = creep.name.split("-");
-        if(name[0] === this.namePrefix && name[1] === String(this.options.id)){
-            quantity++;
-            if(creep.memory.canUpgrade){
-                let status = creep.upgradeController(target);
-                if(status === ERR_NOT_IN_RANGE){
-                    creep.moveTo(target);
-                }else if(status === ERR_NOT_ENOUGH_RESOURCES){
-                    creep.memory.canUpgrade = false;
+        if (missionParams.processData.quantityCreeps < missionParams.requiredCreeps) {
+            spawnerRoom.spawnCreep([MOVE, WORK, CARRY], (this.namePrefix + "-" + this.id + "-" + Math.floor(Math.random() * 100)), {});
+        }
+
+        missionParams.processData.quantityCreeps = 0;
+        for (let i in Game.creeps) {
+            let creep = Game.creeps[i];
+            let name = creep.name.split("-");
+            if (name[0] === this.namePrefix && name[1] === String(this.options.id)) {
+                missionParams.processData.quantityCreeps++;
+
+                if (creep.memory.canUpgrade) {
+                    let status = creep.upgradeController(target);
+                    if (status === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target);
+                    } else if (status === ERR_NOT_ENOUGH_RESOURCES) {
+                        creep.memory.canUpgrade = false;
+                    }
+                } else {
+                    let sources = spawnerRoom.room.find(FIND_SOURCES_ACTIVE);
+                    let status = creep.harvest(sources[0]);
+                    if (status === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0]);
+                    }
                 }
-            }else{
-                let sources = spawnerRoom.room.find(FIND_SOURCES_ACTIVE);
-                let status = creep.harvest(sources[0]);
-                if(status === ERR_NOT_IN_RANGE){
-                    creep.moveTo(sources[0]);
-                }
-            }
 
-            if(creep.store[RESOURCE_ENERGY] === creep.store.getCapacity()){
-                creep.memory.canUpgrade = true;
+                if (creep.store[RESOURCE_ENERGY] === creep.store.getCapacity()) {
+                    creep.memory.canUpgrade = true;
+                }
             }
         }
     }
-    this.quantityCreeps = quantity;
 };
 
 module.exports = {
-    create(name, options){
-        switch (name) {
-            case "A":
-                return new MissionA(options);
+    create(args){
+        switch (args.nameMission) {
+            case "uc":
+                if(args.targetId == null){
+                    throw "targetId is not defined";
+                }
+                let id = Math.floor(Math.random()*100);
+                let mission = {};
+                mission.targetId = args.targetId;
+                mission.requiredCreeps = args.requiredCreeps | 1;
+                Memory.missions[id] = mission;
+                return new MissionUpdateController(id);
         }
     }
 };
