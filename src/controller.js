dabf2eccf8f950ab9c0271703f209f62ece666b4
silentium_noxe <developer.silentium@gmail.com>
2020-07-11 13:46:18 +0300

    new modules

---
+++ src/controller.js
@@ -0,0 +1,75 @@
+function controller(id){
+    if(id == null) return;
+    const target = Game.getObjectById(id);
+    const room = target.room;
+
+    function renew(creep){
+        if(creep.ticksToLive < 500 && room.spawn.store[RESOURCE_ENERGY] >= 100){
+            creep.moveTo(room.spawn, {reusePath: 30, ignoreCreeps: false});
+            room.spawn.renewCreep(creep);
+            return;
+        }
+    }
+
+    function takeEnergy(creep){
+        if(creep.memory.energy == null){
+            const energy = creep.room.find(FIND_STRUCTURES, {filter: (structure) => {
+                if(structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE){
+                    return structure.store[RESOURCE_ENERGY] >= creep.store.getCapacity(RESOURCE_ENERGY);
+                }
+            }});
+
+            let minDistance = 100;
+            let nearEnergy;
+            for(const a of energy){
+                let distance = creep.pos.getRangeTo(a);
+                if(distance < minDistance){
+                    minDistance = distance;
+                    tmpEnergy = a;
+                }
+            }
+
+            if(nearEnergy != null)
+                creep.memory.energy = energy.id;
+        }
+
+        const energy = Game.getObjectById(creep.memory.energy);
+        if(energy != null){
+            creep.moveTo(energy, {reusePath: 10, ignoreCreeps: false});
+            creep.withdraw(energy, RESOURCE_ENERGY);
+        }
+    }
+
+    return {
+        upgrade(creepsQuantity, body=[WORK, CARRY, MOVE], namePrefix="CL"){
+            const creeps = room.find(FIND_MY_CREEPS, {filter: (creep) => creep.name.startsWith(namePrefix)});
+            creeps.forEach((creep) => {
+                renew(creep);
+
+                if(creep.store[RESOURCE_ENERGY] == 0){
+                    takeEnergy(creep);
+                }else{
+                    creep.memory.energy = null;
+
+                    creep.moveTo(target, {reusePath: 50, ignoreCreeps: false});
+                    creep.upgradeController(target);
+                }
+            })
+
+            if(creeps.length < creepsQuantity){
+                room.spawn.spawnCreep(body, namePrefix+"-"+Math.floor(Math.random()*100));
+            }
+        },
+        claim(roomName){
+
+        },
+        reserve(roomName){
+
+        },
+        attack(roomName){
+
+        }
+    }
+}
+
+module.exports = controller;
---
+++ src/harvest.js
@@ -0,0 +1,74 @@
+function harvest(id, namePrefix="H", creepsQuantity=3, body=[WORK, CARRY, MOVE]){
+    if(id == null) return;
+    const source = Game.getObjectById(id);
+    const room = source.room;
+    const creeps = source.room.find(FIND_MY_CREEPS, {filter: (creep) => creep.name.startsWith(namePrefix)});
+
+    function renew(creep){
+        if(creep.ticksToLive < 500 && room.spawn.store[RESOURCE_ENERGY] >= 100){
+            creep.moveTo(room.spawn, {reusePath: 30, ignoreCreeps: false});
+            room.spawn.renewCreep(creep);
+            return;
+        }
+    }
+
+    function harvestTransfer(creep, target){
+        if(creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0){
+            creep.memory.harvest = false;
+        }
+
+        if(creep.memory.harvest){
+            creep.moveTo(source, {reusePath: 10, ignoreCreeps: false});
+            creep.harvest(source);
+        }else{
+            creep.moveTo(target, {reusePath: 30, ignoreCreeps: false});
+            const status = creep.transfer(target, RESOURCE_ENERGY);
+            if(status == ERR_NOT_ENOUGH_ENERGY) creep.memory.harvest = true;
+        }
+    }
+
+    if(creeps.length < creepsQuantity){
+        room.spawn.spawnCreep(body, namePrefix+"-"+Math.floor(Math.random()*100));
+    }
+
+    return {
+        container(id){
+            const target = Game.getObjectById(id);
+
+            creeps.forEach(creep => {
+                renew(creep);
+                harvestTransfer(creep, target);
+            })
+            return harvest(id);
+        },
+        storage(id){
+            const target = Game.getObjectById(id);
+
+            creeps.forEach(creep => {
+                renew(creep);
+                harvestTransfer(creep, target);
+            })
+            return harvest(id);
+        },
+        spawn(id){
+            const target = Game.getObjectById(id);
+
+            creeps.forEach(creep => {
+                renew(creep);
+                harvestTransfer(creep, target);
+            })
+            return harvest(id);
+        },
+        tower(id){
+            const target = Game.getObjectById(id);
+
+            creeps.forEach(creep => {
+                renew(creep);
+                harvestTransfer(creep, target);
+            })
+            return harvest(id);
+        }
+    }
+}
+
+module.exports = harvest;
