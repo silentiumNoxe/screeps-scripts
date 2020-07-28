45b932be8a28180d60da7641fc1c0948b56809d0
silentiumNoxe <silentium.noxe@gmail.com>
2020-07-29 00:28:01 +0300

    refactor

--- src/role.js
+++ src/role.js
@@ -85,7 +85,158 @@ module.exports[Creep.ROLE_HARVESTER] = function(creep){
             }
             else if(status == ERR_NOT_IN_RANGE) creep.moveTo(target);// OPTIMIZE: reusePath, ignoreCreeps
         }
+    }
+}
 
+module.exports[Creep.ROLE_UCL] = function(creep){
+    if(creep.memory.todo == Creep.TODO_ENERGY){
+        let target = Game.getObjectById(creep.memory.energy);
+        if(target == null || target.store[RESOURCE_ENERGY] < creep.store.getCapacity(RESOURCE_ENERGY)){
+            creep.say("👀", true);
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => {
+                if(s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE){
+                    return s.store[RESOURCE_ENERGY] > 0;
+                }
+            }});
+        }
+        if(target == null){
+            creep.wait(200);
+            return;
+        }
 
+        creep.memory.energy = target.id;
+        let status = creep.withdraw(target, RESOURCE_ENERGY);
+        if(status == ERR_FULL || creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) creep.memory.todo = Creep.TODO_UPGRADE;
+        else if(status == ERR_NOT_IN_RANGE) creep.moveTo(target);// OPTIMIZE: reusePath, ignoreCreeps
+        else if(status = OK) creep.say("👇", true);
+    }
+
+    if(creep.memory.todo == Creep.TODO_UPGRADE){
+        let target = creep.room.controller;
+        if(!creep.pos.isNearTo(target))
+            creep.moveTo(target, {visualizePathStyle: {}});// OPTIMIZE: reusePath, ignoreCreeps
+        let status = creep.upgradeController(target);
+        if(status == ERR_NOT_ENOUGH_RESOURCES) creep.memory.todo = Creep.TODO_ENERGY;
+    }
+}
+
+module.exports[Creep.ROLE_BUILDER] = function(creep){
+    if(creep.memory.todo == Creep.TODO_ENERGY){
+        let target = Game.getObjectById(creep.memory.energy);
+        if(target == null || target.store[RESOURCE_ENERGY] < creep.store.getCapacity(RESOURCE_ENERGY)){
+            creep.say("👀", true);
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => {
+                if(s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE){
+                    return s.store[RESOURCE_ENERGY] > 0;
+                }
+            }});
+        }
+
+        if(target == null){
+            creep.wait(200);
+            return;
+        }
+
+        creep.memory.energy = target.id;
+        let status = creep.withdraw(target, RESOURCE_ENERGY);
+        if(status == ERR_FULL || creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0) creep.memory.todo = Creep.TODO_REPAIR;
+        else if(status == ERR_NOT_IN_RANGE) creep.moveTo(target);// OPTIMIZE: reusePath, ignoreCreeps
+        else if(status = OK) creep.say("👇", true);
+    }
+
+    if(creep.memory.todo == Creep.TODO_REPAIR){
+        let target = Game.getObjectById(creep.memory.target);
+        if(target == null || target.hits == target.hitsMax){
+            creep.say("👀", true);
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => {
+                if(s.structureType == STRUCTURE_WALL){
+                     return false;
+                }
+
+                if(s.structureType == STRUCTURE_RAMPART) return s.hits < 1000000;
+
+                if(s.hits < s.hitsMax - (s.hitsMax * 0.1)) return true;
+            }});
+        }
+        if(target != null){
+            creep.say("️🔧", true);
+            creep.memory.target = target.id;
+            let status = creep.repair(target);
+            if(status == ERR_NOT_ENOUGH_RESOURCES) creep.memory.todo = Creep.TODO_ENERGY;
+            else if(status == ERR_NOT_IN_RANGE) creep.moveTo(target);// OPTIMIZE: reusePath, ignoreCreeps
+        }else{
+            creep.memory.todo = Creep.TODO_BUILD;
+        }
+    }
+
+    if(creep.memory.todo == Creep.TODO_BUILD){
+        let target = Game.getObjectById(creep.memory.target);
+        if(target == null){
+            creep.say("👀", true);
+            target = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        }
+        if(target != null){
+            creep.say("🔨", true);
+            creep.memory.target = target.id;
+            let status = creep.build(target);
+            if(status == ERR_NOT_ENOUGH_RESOURCES) creep.memory.todo = Creep.TODO_ENERGY;
+            else if(status == ERR_NOT_IN_RANGE) creep.moveTo(target);// OPTIMIZE: reusePath, ignoreCreeps
+            else if(status == ERR_INVALID_TARGET) creep.memory.target = null;
+        }else{
+            creep.memory.todo = Creep.TODO_REPAIR;
+        }
+    }
+}
+
+module.exports[Creep.ROLE_CLAIMER] = function(creep){
+    if(creep.memory.todo == Creep.TODO_FIND_TARGET){
+        creep.say("👀", true);
+        let target = creep.findClosestByPath(Game.flags, {filter: (f) =>{
+            if(f.name.startsWith("attack")){
+                return f.room.controller.pos.isEqualTo(f.pos);
+            }
+        }});
+        let todo = Creep.TODO_ATTACK;
+        if(target == null){
+            target = creep.findClosestByPath(Game.flags, {filter: (f) => f.name.startsWith("claim")});
+            todo = Creep.TODO_CLAIM;
+        }
+
+        if(target == null){
+            target = creep.findClosestByPath(Game.flags, {filter: (f) =>{
+                if(f.name.startsWith("reserve")){
+                    let ticksToEnd = f.reservation.ticksToEnd;
+                    return f.my == false || (ticksToEnd != null && ticksToEnd < (CONTROLLER_RESERVE_MAX * 0.5));
+                }
+            }});
+            todo = Creep.TODO_RESERVE;
+        }
+
+        if(target == null){
+            todo = Creep.TODO_FIND_TARGET;
+            creep.wait(500);
+        }
+
+        creep.memory.target = target.name;
+        creep.memory.todo = todo;
+    }
+
+    if(creep.memory.todo == Creep.TODO_RESERVE){
+        let target = Game.flags[creep.memory.target];
+        if(target == null){
+            creep.memory.todo = Creep.TODO_FIND_TARGET;
+            return;
+        }
+
+        target = target.room.controller;
+
+        let status = creep.reserveController(target);
+        if(status == ERR_NOT_IN_RANGE){
+            creep.moveTo(target);
+        }
+
+        if(target.reservation.ticksToEnd == CONTROLLER_RESERVE_MAX){
+            creep.memory.todo = Creep.TODO_FIND_TARGET;
+        }
     }
 }
