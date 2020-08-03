ca6816d6d59dd88bc05267cf51cbe3018fbebf19
silentiumNoxe <silentium.noxe@gmail.com>
2020-08-03 19:09:32 +0300

    sql parser

--- src/main.js
+++ src/main.js
@@ -1,3 +1,186 @@
+const harvestFromSim = "CREEP h1, h2, h3 HARVEST ROOM('sim')";
+const harvestFromSim = "CREEP * HARVEST id1, id2, id3 INFINITY";
+const harvestFromSim = "CREEP h1 HARVEST POS(1, 1, 'sim')";
+const spawnCreepQuery = "SPAWNER * SPAWN body=[WORK, CARRY, MOVE], name=h1";
+
+const keywords = [
+    "CREEP", "creep",
+    "SPAWNER", "spawner",
+    "HARVEST", "harvest",
+    "SPAWN", "spawn",
+    "ROOM", "room",
+    "POS", "pos",
+    "BODY", "body",
+    "WORK", "work",
+    "CARRY", "carry",
+    "MOVE", "move",
+    "NAME", "name",
+    "INFINITY", "infinity"
+];
+
+function Query(string){
+    this.string = string;
+    this.commands = findCommands(string);
+    this.type = null;
+    this.creeps = [];
+    let ptr = 0;
+
+    // первое слово определяет тип запроса
+    let type = this.commands[ptr].toUpperCase();
+    if(type == "CREEP"){
+        this.type = type = 0;
+    }else if(type == "SPAWNER"){
+        this.type = type = 1;
+    }else{
+        throw new Error("Wrong keyword >"+this.commands[ptr]+". Should be CREEP or SPAWNER");
+    }
+
+    ptr = 1;
+
+    //todo: add spawner logic
+    if(type == 0){
+        let creepName = this.commands[ptr];
+        let isKeyword = keywords.indexOf(creepName) > -1;
+        if(isKeyword){
+            throw new Error("Wrong creep name >"+creepName);
+        }
+
+        if(creepName == "*"){
+            isKeyword = true;
+            this.creeps.push("*");
+            ptr++;
+        }
+
+        while(!isKeyword){
+            this.creeps.push(creepName);
+            creepName = this.commands[++ptr];
+            isKeyword = keywords.indexOf(creepName) > 1;
+        }
+
+        let action = this.commands[ptr];
+        if(action.toUpperCase("HARVEST")){
+            this.action = "harvest";
+
+            this.sources = [];
+            let sourceId = this.commands[++ptr];
+            isKeyword = keywords.indexOf(sourceId) > -1;
+            if(isKeyword){
+                throw new Error("Wrong source >"+this.commands[ptr]);
+            }
+
+            while(!isKeyword){
+                this.sources.push(sourceId);
+                sourceId = this.commands[++ptr];
+                isKeyword = keywords.indexOf(sourceId) > -1;
+            }
+
+            ptr--;
+        }else if(action.toUpperCase("TRANSFER")){
+            this.action = "transfer";
+            this.targets = [];
+
+            let targetId = this.commands[++ptr];
+            isKeyword = keywords.indexOf(targetId) > -1;
+            if(isKeyword){
+                throw new Error("Wrong target >"+this.commands[ptr]);
+            }
+
+            while(!isKeyword){
+                this.targets.push(targetId);
+                targetId = this.commands[++ptr];
+                isKeyword = keywords.indexOf(targetId) > -1;
+            }
+
+            ptr--;
+        }
+
+        let infinity = this.commands[++ptr];
+        if(infinity.toUpperCase() == "INFINITY"){
+            this.infinity = true;
+        }
+    }else if(type == 1){
+        let spawnName = this.commands[ptr];
+        let isKeyword = keywords.indexOf(spawnName) > -1;
+        if(isKeyword){
+            throw new Error("Wrong spawn name >"+spawnName);
+        }
+
+
+    }
+}
+
+function findCommands(string){
+    let commands = [];
+
+    let buff = "";
+    let isFunc = false;
+    for(let i = 0; i < string.length; i++){
+        let char = string.charAt(i);
+        buff += char;
+
+        if(char == " " && !isFunc || (i == string.length-1)){
+            buff = buff.replace(" ", "");
+            buff = buff.replace(",", "");
+            commands.push(buff);
+            buff = "";
+            isFunc = false;
+            continue;
+        }
+
+        if(char == "("){
+            isFunc = true;
+        }else if(char == ")"){
+            commands.push(buff);
+            buff = "";
+            isFunc = true;
+            continue;
+        }
+    }
+
+    return commands;
+}
+
+function funcPos(val){
+    if(!val.startsWith("ROOM(")) return null;
+
+
+}
+
+
+
+Query.prototype.next = function(){
+    return this.commands[this.ptr++];
+}
+
+Query.prototype.hasNext = function(){
+    return this.commands[this.ptr+1] != null;
+}
+
+Query.prototype.command = function(){
+    return this.commands[this.ptr];
+}
+
+Query.TYPE = {
+    CREEP: 0,
+    SPAWNER: 1
+}
+
+Query.KEY = {
+    CREEP: "CREEP",
+    HARVEST: "HARVEST",
+    SPAWNER: "SPAWNER"
+}
+
+global.$ = function(query){
+    const qo = new Query(query);
+    if(qo.type == Query.KEY.CREEP){
+        this.task.creep = qo.next();
+        if(!this.hasNext()){
+            throw new Error("Wrong query");
+        }
+    }
+}
+
 global.query = function(task){
     let taskObj = {};
     const commands = task.split(" ");
