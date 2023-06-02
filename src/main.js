4e2d4274580e9c5fb051069d258310c49b3225ae
silentium.noxe <silenium.noxe@gmail.com>
2023-06-03 01:27:29 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -1,47 +1,5 @@
-require("constants");
-const Func = require("Func");
-const Query = require("Query");
+function loop() {
 
-const keywords = [
-    "CREEP", "creep",
-    "SPAWNER", "spawner",
-    "HARVEST", "harvest",
-    "SPAWN", "spawn",
-    "ROOM", "room",
-    "POS", "pos",
-    "BODY", "body",
-    "WORK", "work",
-    "CARRY", "carry",
-    "MOVE", "move",
-    "NAME", "name",
-    "INFINITY", "infinity"
-];
-
-global.$ = function(query){
-    const qo = new Query(query);
-    console.log(JSON.stringify(qo));
-    if(Memory.queries == null) Memory.queries = [];
-    Memory.queries.push(qo);
 }
 
-module.exports.loop = function () {
-    if(Memory.queries == null) Memory.queries = [];
-
-    for(let i = 0; i < Memory.queries.length; i++){
-        const q = Memory.queries[i];
-
-        if(q.type == null) throw new Error("Wrong query type "+q.type);
-
-        if(q.type == 0){//creep
-            const creeps = Func.creeps(q.creeps);
-            if(creeps.length == 0) throw new Error("Creeps not found ("+q.string+")");
-
-            if(q.action == "harvest"){
-                const sources = Func.sources(q.sources);
-                creeps.forEach((creep) => Func.harvest(creep, sources));
-            }else if(q.action == "transfer"){
-
-            }
-        }
-    }
-}
+module.exports.loop = loop;
